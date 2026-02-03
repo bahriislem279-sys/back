a0106f245beb2c7530df3c542e65ecabb2580d2f
DARIOO33 <darioanwar3@gmail.com>
2026-02-03 17:08:24 +0100

    add get Single form route

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -2,10 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose")
 const Form = require("../models/Form")
 const router = express.Router()
-
+//get all forms
 router.get("/", async (req, res) => {
     try {
         const form = await Form.find();
+        return res.status(200).json(form)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Server error" })
+    }
+})
+
+//get form by id
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const form = await Form.findById(id);
+        if (!form) {
+            return res.status(404).json({ message: "Not Found" })
+        }
         return res.status(200).json(form)
     } catch (error) {
         console.log(error)
