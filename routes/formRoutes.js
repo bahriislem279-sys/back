2c8a6b8f8c99ee52ad8ced36adafeb008b5b974a
bahriislem279-sys <bahriislem279@gmail.com>
2026-02-06 16:55:28 +0100

    add delete form

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -108,6 +108,22 @@ router.patch(
     }
   },
 );
+router.delete("/:id", requireAuth, requireRole("admin"), async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+  try {
+    const deletedForm = await Form.findByIdAndDelete(id);
+    if (!deletedForm) {
+      return res.status(404).json({ message: "Not Found" });
+    }
+    return res.status(200).json({ message: "Form deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 module.exports = {
   formRoutes: router,
 };
